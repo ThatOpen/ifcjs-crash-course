c4b34a660bc093f33b327be41b64b36416d0195a
Ángel Díaz <angel_diaz_murillo@hotmail.com>
2022-05-24 19:56:28 +0200

    Update operators example

--- exercises/JavaScript/Operators/index.js
+++ exercises/JavaScript/Operators/index.js
@@ -1,8 +1,15 @@
-let pillarHeight = 3.50 //m
-let pillarBaseArea = 0.5 * 0.5 //m2
+let pillarHeight = 3.5 //m
+let pillarAreaBase = 0.5*0.5 //m2
 
-let pillarVolume = pillarHeight * pillarBaseArea //m3
-const weightReinforcement = 1.5 //Kg 
+let pillarVolume = pillarHeight * pillarAreaBase //m3
+const weightReinforcement = 1.5 //kg
 
-let reiforcementRatio = (pillarVolume == 0) || weightReinforcement/pillarVolume
-console.log(reiforcementRatio)
+let reinforcementRatio = (pillarVolume == 0) || weightReinforcement/pillarVolume
+console.log(reinforcementRatio)
+
+pillarHeight = 0 //m
+pillarVolume = pillarHeight * pillarAreaBase //m3
+
+
+reinforcementRatio = (pillarVolume == 0) || weightReinforcement/pillarVolume
+console.log(reinforcementRatio)
