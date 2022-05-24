97996afa454868f5948edb4084722d07b640e84a
Ángel Díaz <angel_diaz_murillo@hotmail.com>
2022-05-24 17:11:59 +0200

    Add Variables and Constants example

---
+++ exercises/JavaScript/Variables and Constants/index.js
@@ -0,0 +1,9 @@
+let studentName = 'Angel'
+console.log(studentName)
+
+studentName = 'Juan'
+console.log(studentName)
+
+const studentNameSet = studentName
+studentName = 'Fernando'
+console.log(studentNameSet, studentName )
