ddcd3a07dfb226083ff94119d56c724af5bbefa7
Ángel Díaz <angel_diaz_murillo@hotmail.com>
2022-05-27 20:30:07 +0200

    Update Primitive Data Type

---
+++ exercises/JavaScript/Primitive Data Type/app.js
@@ -0,0 +1,16 @@
+let profName = 'Antonio'
+let profSurName = ' Gonzalez'
+
+let var1 = 20
+let var2 = 8
+
+let sum = var1 + var2
+console.log(sum)
+
+sum = profName + profSurName
+console.log(sum)
+
+age = (var1 + var2).toString()
+
+let presentation = "Hi, I'm " + sum + ", and I'm " + age + ' years old'
+console.log(presentation)
